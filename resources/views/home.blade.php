5f67875e116213474961cbcbfb3e07d33d536353
Erik Westlund <edbwestlund@gmail.com>
2016-11-10 18:57:42 -0600

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
 @section('content')
         <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
-                    Documenting Hate
+                <div class="col-md-12 text-center">
+                    <h1>Documenting Hate</h1>
                 </div>
 
-                <div class="title m-b-md" style="font-size: 1.5em;">
-                    Coming Soon
+                <div class="title col-md-12 text-center">
+                    <h2>Coming Soon</h2>
                 </div>
 
 
