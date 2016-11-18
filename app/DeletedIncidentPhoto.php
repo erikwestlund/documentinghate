e8bd7081fb1b35eae5d430f6e3635c1dbfd8a9ba
Erik Westlund <edbwestlund@gmail.com>
2016-11-17 23:53:51 -0600

    touches for cache busting

--- app/DeletedIncidentPhoto.php
+++ app/DeletedIncidentPhoto.php
@@ -2,10 +2,16 @@
 
 namespace App;
 
+use Laracasts\Matryoshka\Cacheable;
+
 use Illuminate\Database\Eloquent\Model;
 
 class DeletedIncidentPhoto extends Model
 {
+    use Cacheable;
+    
+    protected $touches = ['incident'];    
+
     public function user()
     {
         return $this->belongsTo('App\User');
--- app/IncidentModerationDecision.php
+++ app/IncidentModerationDecision.php
@@ -2,10 +2,15 @@
 
 namespace App;
 
+use Laracasts\Matryoshka\Cacheable;
 use Illuminate\Database\Eloquent\Model;
 
 class IncidentModerationDecision extends Model
 {
+    use Cacheable;
+    
+    protected $touches = ['incident'];    
+
     public function user()
     {
         return $this->belongsTo('App\User');
