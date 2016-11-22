14065696e640613931a0d4fa8d6cd42980e405e4
Erik Westlund <edbwestlund@gmail.com>
2016-11-22 14:36:15 -0600

    css fixes

--- resources/views/_mini-menu.blade.php
+++ resources/views/_mini-menu.blade.php
@@ -1,18 +1,14 @@
 <div class="col-sm-10 col-sm-offset-1  top-margin-lg bottom-margin-lg mini-menu">
 
-    <div class="col-sm-4">
-        <div class="document-incident text-center remembering-love">
-            <a href="{{ url('/add') }}"><i class="fa fa-heart"></i> Submit an Act</a>
-        </div>
+    <div class="col-sm-4 document-incident text-center">
+        <a href="{{ url('/add') }}"><i class="fa fa-pencil"></i> Document an Incident</a>
     </div>        
-    <div class="col-sm-4 text-center">
+    <div class="col-sm-4 text-center search-box">
         <form method="GET" action="{{ url('/')}}">
             <input type="text" name="search" placeholder="Search" class="form-control search"><i class="search-icon fa fa-search"></i>
         </form>
     </div>
-    <div class="col-sm-4 text-center">
-        <div class="documenting-hate">   
-            <a href="http://doucmentinghateinthe.us/"><i class="fa fa-pencil"></i> Documenting Hate</a>
-        </div>
+    <div class="col-sm-4 text-center remembering-love">
+        <a href="http://rememberingloveinthe.us/"><i class="fa fa-heart"></i> Remembering Love</a>
     </div>
 </div>
