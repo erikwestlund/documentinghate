44ecf89173195878de16a51b1eaff6181e5664ea
Erik Westlund <edbwestlund@gmail.com>
2016-11-22 12:31:45 -0600

    User registration notification

--- app/Http/Observers/UserObserver.php
+++ app/Http/Observers/UserObserver.php
@@ -2,8 +2,11 @@
 
 namespace App\Observers;
 
+use Notification;
+
 use App\Role;
 use App\User;
+use App\Notifications\UserRegistered;
 
 class UserObserver
 {
@@ -20,6 +23,21 @@ class UserObserver
         if($user->id != 1) {
             $user->attachRole(Role::where('name', 'user')->first());
         }
+
+        $this->notifyAdministratorsOfNewUser($user);
+    }
+
+    /**
+     * When a new user registeres, notify the administrators.
+     * @param  User $user 
+     * @return Void
+     */
+    protected function notifyAdministratorsOfNewUser($user)
+    {
+        $users_to_notify = User::administrators()->
+            get();
+
+        Notification::send($users_to_notify, new UserRegistered($user));
     }
 
 }
