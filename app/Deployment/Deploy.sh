af91ec672f968822cbef66789960af1a627a11fb
Erik Westlund <edbwestlund@gmail.com>
2016-11-22 14:57:25 -0600

    fixes

--- app/Deployment/Deploy.sh
+++ app/Deployment/Deploy.sh
@@ -7,11 +7,6 @@ TITLE="Documenting Hate"
 # Go to app directory
 cd $APP_DIR
 
-# Fix directory permissions
-chown -R www-data:www-data $APP_DIR
-find $APP_DIR -type f -exec chmod 664 {} \;    
-find $APP_DIR -type d -exec chmod 775 {} \;
-
 # Enter Maintenance Mode
 php artisan down --message='$TITLE will be back in a minute. Please retry then.' --retry=60
 
