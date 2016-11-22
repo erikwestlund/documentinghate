3fee722e7327939c1ca6d8285108e3f2de8f92d4
Erik Westlund <edbwestlund@gmail.com>
2016-11-22 12:39:33 -0600

    user registered notifier

---
+++ app/Notifications/UserRegistered.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use App\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserRegistered extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('A user registered: '. $this->user->name . ' (' . $this->user->email . ')')
+                    ->line('You may want to give them moderator privileges. Or you might not.')
+                    ->action('Check \'Em Out', url('/admin/users/' . $this->user->id));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
