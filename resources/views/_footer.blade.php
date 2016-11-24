9cc8a088d4d959c3f08455c3d503e319cf2d8848
Erik Westlund <edbwestlund@gmail.com>
2016-11-24 10:38:56 -0600

    narrower footer

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,7 +1,7 @@
 @if(! Request::is('about'))
     
     <div class="container footer">
-        <div class="col-sm-12 top-border top-margin-md top-padding-md bottom-padding-lg">
+        <div class="col-sm-10 col-sm-offset-1 top-border top-margin-md top-padding-md bottom-padding-lg">
             <a href="{{ secure_url('/about') }}"> About {{ config('site.title') }}</a>
         </div>
     </div>
