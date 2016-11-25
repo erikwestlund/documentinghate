b215112530587c02b73affa7c5c2f257ddc880ca
Erik Westlund <edbwestlund@gmail.com>
2016-11-25 10:36:31 -0600

    footer fix

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,4 +1,4 @@
-@if(! Request::is('about'))
+@if(! Request::is('about') && !Request::is('admin*') )
     
     <div class="container footer">
         <div class="col-sm-10 col-sm-offset-1 top-border top-margin-md top-padding-md bottom-padding-lg">
